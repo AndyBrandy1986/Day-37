1e4c134f53b9d7a34707bc1250bf1232e02bba54
Андрей Бредихин <72138285+AndyBrandy1986@users.noreply.github.com>
2023-02-06 17:34:09 +0200

    Не работает конкретно то что не закомментировано

---
+++ exercise.js
@@ -0,0 +1,58 @@
+// Practice what you learned!
+
+// 1) Select the two <button> elements and store them in two different variables.
+//    - Select the first button without adding or using any "id"
+
+// let firstButtonElement = document.querySelector("button");
+// function consoleDirremPar() {
+//   console.dir(firstButtonElement);
+// }
+
+//    - Select the second button by using an "id"
+
+// let secondButtonElement = document.getElementById("blue-background");
+
+//function function2(event) {
+//  console.dir(event.target);
+//}
+
+// 2) Add "click" event listener to both buttons (with two different functions).
+//    The functions should "console.dir()" the clicked buttons.
+//    - Output the first button by using the variable in which it's stored
+
+// firstButtonElement.addEventListener("click", delFirstPar);
+
+//    - Output the second button WITHOUT using the variable in which it's stored
+
+// secondButtonElement.addEventListener("click", function2);
+
+// 3) Now select and store the paragraphs mentioned in the text you see on the page
+//    (first and third paragraph)
+
+//    - Select BOTH paragraphs by drilling into the document and "navigating" to the
+//      mentioned elements
+
+const firstPharaghElement = document.body.children[2].children[1];
+
+function changeBckgClr() {
+  firstPharaghElement.style.backgroundColor = "blue";
+}
+
+firstPharaghElement.addEventListener("click", changeBckgClr);
+
+//    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
+// 4) Change the functions from (2) such that:
+//    - The first button removes the third paragraph (i.e. the <p> prior to it)
+
+const thirdPharagraphElement = document.body.children[2].children[3];
+
+function delFirstPar() {
+  thirdPharagraphElement.remove();
+}
+
+thirdPharagraphElement.addEventListener("click", delFirstPar);
+
+//    - The second button changes the background color of the first paragraph to blue
+
+// 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
+//    Note: You'll have to add those classes to the styles.css file first!
